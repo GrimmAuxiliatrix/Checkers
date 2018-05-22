0801a87732ab214c71e5aa9aca1ba97af933221c
alawngnome <32248903+alawngnome@users.noreply.github.com>
2018-05-22 17:23:04 -0400

    Add files via upload

---
+++ MainMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Canvas MainMenuCanvas;
+	public Canvas HowToCanvas;
+
+
+	void Start () {
+		HowToCanvas.enabled = false;	
+	}
+		
+	public void PlayOn() {
+		//Open Game Scene
+	}
+	public void HowToOn() {
+		HowToCanvas.enabled = true;
+		MainMenuCanvas.enabled = false;
+	}
+	public void ReturnOn() {
+		HowToCanvas.enabled = false;
+		MainMenuCanvas.enabled = true;
+	}
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
