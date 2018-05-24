557fbeea693b17a2b0fc87a03b7aec106641d03b
salongaale <salongaale@gmail.com>
2018-05-23 22:43:36 -0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//I honestly don't know if we even need this script but in this world it's yeet or get yeeted
+public class Player : MonoBehaviour {
+	GameObject[,] board; 
+	GameObject selected; //piece that player selected
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//i and j in this method represent the x and y directions of the board dont really know whats the parameters that go here
+	bool canMove(GameObject[i,j] board){
+		if(i > 0 && i<8 && j > 0  && j< 8){
+			if( 
+	}
+}
