0b1d467885fed2975ea0dac1ef3440dbf21549de
alawngnome <32248903+alawngnome@users.noreply.github.com>
2018-05-29 23:08:58 -0400

    added a Capture checking function

--- PieceMovement.cs
+++ PieceMovement.cs
@@ -23,12 +23,13 @@ public class PieceMovement : MonoBehaviour {
 		{0,3,0,3,0,3,0,3}
 	};
 
-	private bool CanMove(int posX, int posY, int targetX, int targetY) {
-		int pieceType = board [posX, posY]; // returns the type of piece that's being targeted in int form
+
+	private bool CanMove(int posX, int posY, int targetX, int targetY) { // tests whether a move is valid or not
+		int pieceType = board [posX, posY]; // returns the type of piece that's being selected in int form
 
 		if (board [targetX, targetY] != 0) { // checks that the target location is empty
 			return false;
-		} else if ((pieceType == 2) || (pieceType == 4)) { // Kings can have negative and position change in y position
+		} else if ((pieceType == 2) || (pieceType == 4)) { // Kings can have negative and positive change in y position
 			if ((Mathf.Abs (targetX - posX)) != 2 || Mathf.Abs (targetY - posY) != 2) { 
 				return false; // checks that the target location is two spots away diagonally in any direction
 			}
@@ -41,6 +42,22 @@ public class PieceMovement : MonoBehaviour {
 				return false; // checks that the target location is two spots away upward
 			}
 		} 
+		return true;
+	}
+	private bool CanCapture(int posX, int posY, int targetX, int targetY) {
+		int pieceType = board [posX, posY];
+		int victimPiece = board [((posX + targetX) / 2), ((posY + targetY) / 2)];
+
+		if (CanMove (posX, posY, targetX, targetY) == true) { // CanMove will detect whether a piece is blocking a capture
+			return false;
+		} else if (victimPiece != 0) { // checking that you're not marking jumping over a empty spot as a kill; for use on UI
+			return false;
+		}
+		else if (pieceType >= 3 && (victimPiece >= 3)) {
+			return false; // checks that the selected piece and the potential capture are not both red
+		} else if (pieceType <= 2 && (victimPiece <= 2)) {
+			return false; // ditto, but this time for black
+		}
 		return true;
 	}
 }
