3b5aa0bdb11b73abe15c40b1047d3a854452f971
alawngnome <32248903+alawngnome@users.noreply.github.com>
2018-05-30 23:01:51 -0400

    functions not complete

--- PieceMovement.cs
+++ PieceMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PieceMovement : MonoBehaviour {
 
+	public GameObject selectedpiece; //the piece that moves
+
 	/*
 	 1 : Black Man
 	 2 : Black King
@@ -23,6 +25,27 @@ public class PieceMovement : MonoBehaviour {
 		{0,3,0,3,0,3,0,3}
 	};
 
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	// this is for dragging
+	void OnMouseDown(){
+		selectedpiece = this.gameObject;
+		int pieceX = (int) ((selectedpiece.transform.position.x / 1.5) - 1);
+		int pieceY = (int)((selectedpiece.transform.position.x / 1.5) - 1);
+
+		if (board [pieceX, pieceY] <= 2) {
+		}
+
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	void OnMouseDrag(){
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		transform.position = cursorPosition;
+	}
+	// this is for dragging
 
 	private bool CanMove(int posX, int posY, int targetX, int targetY) { // tests whether a move is valid or not
 		int pieceType = board [posX, posY]; // returns the type of piece that's being selected in int form
@@ -30,15 +53,15 @@ public class PieceMovement : MonoBehaviour {
 		if (board [targetX, targetY] != 0) { // checks that the target location is empty
 			return false;
 		} else if ((pieceType == 2) || (pieceType == 4)) { // Kings can have negative and positive change in y position
-			if ((Mathf.Abs (targetX - posX)) != 2 || Mathf.Abs (targetY - posY) != 2) { 
+			if ((Mathf.Abs (targetX - posX)) != 1 || Mathf.Abs (targetY - posY) != 1) { 
 				return false; // checks that the target location is two spots away diagonally in any direction
 			}
 		} else if (pieceType == 1) { // 1 means black man, so the y coordinate change can only be negative(black men can only move downwards)
-			if ((Mathf.Abs (targetX - posX)) != 2 || (targetY - posY != -2)) { 
+			if ((Mathf.Abs (targetX - posX)) != 1 || (targetY - posY != -1)) { 
 				return false; // checks that the target location is two spots away downward
 			}
 		} else if (pieceType == 3) { // 3 means red man, so the y coordinate change can only be positive(red men can only move upwards)
-			if ((Mathf.Abs (targetX - posX)) != 2 || (targetY - posY != 2)) {
+			if ((Mathf.Abs (targetX - posX)) != 1 || (targetY - posY != 1)) {
 				return false; // checks that the target location is two spots away upward
 			}
 		} 
@@ -57,7 +80,26 @@ public class PieceMovement : MonoBehaviour {
 			return false; // checks that the selected piece and the potential capture are not both red
 		} else if (pieceType <= 2 && (victimPiece <= 2)) {
 			return false; // ditto, but this time for black
-		}
+		} 
+		//repeat of CanMove, this time testing a jump taking two spaces instead of one (a kill)
+		else if ((pieceType == 2) || (pieceType == 4)) { // Kings can have negative and positive change in y position
+			if ((Mathf.Abs (targetX - posX)) != 2 || Mathf.Abs (targetY - posY) != 2) { 
+				return false; // checks that the target location is two spots away diagonally in any direction
+			}
+		} else if (pieceType == 1) { // 1 means black man, so the y coordinate change can only be negative(black men can only move downwards)
+			if ((Mathf.Abs (targetX - posX)) != 2 || (targetY - posY != -2)) { 
+				return false; // checks that the target location is two spots away downward
+			}
+		} else if (pieceType == 3) { // 3 means red man, so the y coordinate change can only be positive(red men can only move upwards)
+			if ((Mathf.Abs (targetX - posX)) != 2 || (targetY - posY != 2)) {
+				return false; // checks that the target location is two spots away upward
+			}
+		} 
 		return true;
+	}
+	public void Move(int posX, int posY, int targetX, int targetY) {
+		if (CanMove(posX,posY,targetX,targetY) == true && CanCapture(posX,posY,targetX,targetY) == true) {
+			//wtffff
+		}
 	}
 }
