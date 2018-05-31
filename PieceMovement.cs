7ac5d254b9e60164ed22523daa4d2653ad1b91f9
alawngnome <32248903+alawngnome@users.noreply.github.com>
2018-05-31 15:42:48 -0400

    flash drive just broke; uploading what i have so far

--- PieceMovement.cs
+++ PieceMovement.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class PieceMovement : MonoBehaviour {
 
-	public GameObject selectedpiece; //the piece that moves
+	public GameObject selectedpiece; // the piece that moves
+	public GameObject selectedtile; // the tile that the piece will move to
+	private GameObject pieceInTransition; // neccessary for pieces to move in OnMouseDown 
 
 	/*
 	 1 : Black Man
@@ -28,25 +26,30 @@ public class PieceMovement : MonoBehaviour {
 	private Vector3 screenPoint;
 	private Vector3 offset;
 
-	// this is for dragging
+	// this is for moving
 	void OnMouseDown(){
-		selectedpiece = this.gameObject;
-		int pieceX = (int) ((selectedpiece.transform.position.x / 1.5) - 1);
-		int pieceY = (int)((selectedpiece.transform.position.x / 1.5) - 1);
+		bool pieceselected = false;
+		int pieceX; int pieceY; int boardX; int boardY;
+		if (this.gameObject.name == "Piece" && pieceselected == false) { // for selecting a piece
+			pieceselected = true;
+			pieceX = (int) ((selectedpiece.transform.position.x / 1.5) - 1);
+			pieceY = (int) ((selectedpiece.transform.position.x / 1.5) - 1); // getting coordinates of piece when piece is selected
+			pieceInTransition = this.gameObject;
 
-		if (board [pieceX, pieceY] <= 2) {
 		}
-
-		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		else if(this.gameObject.name == "Board" && pieceselected == true) { // for picking a board spot to move after a piece is selected
+			boardX = (int) ((selectedpiece.transform.position.x / 1.5) - 1);
+			boardY = (int) ((selectedpiece.transform.position.x / 1.5) - 1); // getting coordinates of board when board is selected
+			if (CanMove (pieceX, pieceY, boardX, boardY) == true && board [boardX, boardY] == 0) {
+				board [boardX, boardY] = board [pieceX, pieceY]; //moves the piece to the targeted location
+				board [pieceX, pieceY] = 0; //vacates the spot the moving piece once was
+				//pieceInTransition.transform.position.x = selectedpiece.transform.position.x;
+				//pieceInTransition.transform.position.y = selectedpiece.transform.position.y; //moves the actual piece to the valid spot
+				Vector2 stupidcsharp = new Vector2Int (((int)selectedpiece.transform.position.x), ((int)selectedpiece.transform.position.y));
+				pieceInTransition.transform = stupidcsharp;
+			}
+		}
 	}
-	void OnMouseDrag(){
-		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
-		transform.position = cursorPosition;
-	}
-	// this is for dragging
-
 	private bool CanMove(int posX, int posY, int targetX, int targetY) { // tests whether a move is valid or not
 		int pieceType = board [posX, posY]; // returns the type of piece that's being selected in int form
 
@@ -54,15 +57,15 @@ public class PieceMovement : MonoBehaviour {
 			return false;
 		} else if ((pieceType == 2) || (pieceType == 4)) { // Kings can have negative and positive change in y position
 			if ((Mathf.Abs (targetX - posX)) != 1 || Mathf.Abs (targetY - posY) != 1) { 
-				return false; // checks that the target location is two spots away diagonally in any direction
+				return false; // checks that the target location is one spot away diagonally in any direction
 			}
 		} else if (pieceType == 1) { // 1 means black man, so the y coordinate change can only be negative(black men can only move downwards)
 			if ((Mathf.Abs (targetX - posX)) != 1 || (targetY - posY != -1)) { 
-				return false; // checks that the target location is two spots away downward
+				return false; // checks that the target location is one spot away downward
 			}
 		} else if (pieceType == 3) { // 3 means red man, so the y coordinate change can only be positive(red men can only move upwards)
 			if ((Mathf.Abs (targetX - posX)) != 1 || (targetY - posY != 1)) {
-				return false; // checks that the target location is two spots away upward
+				return false; // checks that the target location is one spot away upward
 			}
 		} 
 		return true;
@@ -102,4 +105,14 @@ public class PieceMovement : MonoBehaviour {
 			//wtffff
 		}
 	}
+	/*
+	  	void OnMouseDown(){
+			selectedpiece = this.gameObject;
+			int pieceX = (int) ((selectedpiece.transform.position.x / 1.5) - 1);
+			int pieceY = (int) ((selectedpiece.transform.position.x / 1.5) - 1);
+
+			if (board [pieceX, pieceY] <= 2) {
+			}
+		}
+		*/
 }
